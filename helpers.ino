c0891f5153cce4bcc007a4cb309bcba4d1bc9921
Lauri Saar <lauri.saar@outlook.com>
2019-10-31 23:12:24 +0200

    #Changed: code style refactor.

--- helpers.ino
+++ helpers.ino
@@ -63,8 +63,8 @@ char *getWifiHostname()
 char *getWiFiMACHex()
 {
   uint8_t mac[6];
-  char macStr[13] = {0};
+  char result[13] = {0};
   WiFi.macAddress(mac);
-  sprintf(macStr, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return macStr;
+  sprintf(result, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return result;
 }
